b8288109cc0c549ec931bd3dc899616b6f0b0b1d
OMARM100 <om7819131@gmail.com>
2026-09-07 21:24:54 +0000

    kernel: validate BootInfo and show clear boot success message

--- kernel/arch/x86_64/entry.asm
+++ kernel/arch/x86_64/entry.asm
@@ -4,28 +4,39 @@ global _start
 section .text.boot
 _start:
     cli
-    mov rsp, 0x0000000000088000
 
-    ; RDI contains BootInfo from the bootloader.
+    ; RDI contains struct skos_bootinfo * from the BIOS bootloader.
+    ; Validate the ABI before doing anything else in the kernel.
+    test rdi, rdi
+    jz .halt
+    cmp dword [rdi], 0x534B4249 ; "SKBI"
+    jne .halt
+    cmp word [rdi + 4], 1
+    jne .halt
+    cmp word [rdi + 6], 64
+    jne .halt
+
+    mov rsp, 0x00000000000B0000
+
     mov rax, 0xB8000
-    mov word [rax], 0x074B
-    mov word [rax + 2], 0x076F
-    mov word [rax + 4], 0x074B
-    mov word [rax + 6], 0x0741
-    mov word [rax + 8], 0x074D
-    mov word [rax + 10], 0x0744
-    mov word [rax + 12], 0x073A
-    mov word [rax + 14], 0x0200
-    mov word [rax + 16], 0x074C
-    mov word [rax + 18], 0x076F
-    mov word [rax + 20], 0x074E
-    mov word [rax + 22], 0x0747
-    mov word [rax + 24], 0x074D
-    mov word [rax + 26], 0x074F
-    mov word [rax + 28], 0x0744
-    mov word [rax + 30], 0x0745
-    mov word [rax + 32], 0x0200
+    mov word [rax + 0], 0x0753 ; S
+    mov word [rax + 2], 0x076B ; k
+    mov word [rax + 4], 0x074F ; O
+    mov word [rax + 6], 0x0753 ; S
+    mov word [rax + 8], 0x0720 ; space
+    mov word [rax + 10], 0x0742 ; B
+    mov word [rax + 12], 0x076F ; o
+    mov word [rax + 14], 0x074F ; O
+    mov word [rax + 16], 0x074C ; L
+    mov word [rax + 18], 0x0720 ; space
+    mov word [rax + 20], 0x074F ; O
+    mov word [rax + 22], 0x074B ; K
 
 .hang:
     hlt
     jmp .hang
+
+.halt:
+    cli
+    hlt
+    jmp .halt
